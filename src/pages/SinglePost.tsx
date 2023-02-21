bb142f26cdfd39ed32989896a12bfaae446331b3
Steffen Samseth <steffen221191@gmail.com>
2023-02-21 15:44:16 +0100

    Refactor single post page
    Moved error checking into the rendering code. Also added a nice loading
    spinner.

--- src/pages/SinglePost.tsx
+++ src/pages/SinglePost.tsx
@@ -7,6 +7,7 @@ import ArrowLeft from "../components/icons/ArrowLeft";
 import Layout from "../components/Layout";
 import Reply from "../components/icons/Reply";
 import Post from "../components/Post";
+import LoadingSpinner from "../components/icons/LoadingSpinner";
 
 const SinglePost: FunctionComponent = () => {
   const { id: postId } = useParams();
@@ -14,67 +15,77 @@ const SinglePost: FunctionComponent = () => {
     return await fetchSinglePost(Number(postId));
   });
 
-  if (query.isLoading) return <div className="text-white">Loading...</div>;
+  const post = query.data;
 
-  if (query.isError)
-    return (
-      <div className="text-white">{`An error has occured ${query.error}`}</div>
-    );
+  const pageTitle = () => {
+    if (query.isLoading) return "Shouter";
 
-  if (!query.data) {
-    return (
-      <Layout>
-        <div className="mb-1 flex flex-row p-6">
-          <Link to="/">
-            <ArrowLeft className="h-6 fill-white pr-8" />
-          </Link>
-          <h1 className="font-bold text-white">404 post not found</h1>
-        </div>
-      </Layout>
-    );
-  }
+    if (query.data) return query.data.title;
 
-  const post = query.data!;
+    return "404 Post Not Found";
+  };
 
   return (
-    <Layout title={post.title}>
+    <Layout title={pageTitle()}>
       <div className="mb-1 flex flex-row p-6">
         <Link to="/">
           <ArrowLeft className="h-6 fill-white pr-8" />
         </Link>
         <h1 className="font-bold text-white">Shout</h1>
       </div>
-      <Post post={post} clickable={false} />
-      <div className="mb-4 p-4 text-white">Comments</div>
-      {post.comments.map((comment) => (
-        <div className="mb-6 pl-4" key={post.id}>
-          <div className="flex items-center">
-            <Link to={`/profiles/${comment.author.name}`}>
-              <div className="aspect-square w-8 overflow-hidden rounded-full">
-                <img
-                  className="h-full w-full object-cover object-center"
-                  src={
-                    comment.author.avatar ||
-                    "../../public/img/default-profile-picture.png"
-                  }
-                />
-              </div>
-            </Link>
 
-            <Link
-              to={`/profiles/${comment.author.name}`}
-              className="ml-4 font-bold text-white"
-            >
-              {comment.author.name}
-            </Link>
-            <Reply className="ml-4 h-4 fill-white"></Reply>
-            <span className="ml-1 text-white">
-              <TimeAgo datetime={comment.created} />
-            </span>
-          </div>
-          <div className="mt-2 text-white">{comment.body}</div>
+      {query.isError && (
+        <div className="text-white">{`An error has occured ${query.error}`}</div>
+      )}
+
+      {query.isSuccess && post === null && (
+        <div className="my-24 text-center text-3xl font-bold text-white">
+          This post does not exist
         </div>
-      ))}
+      )}
+
+      {query.isLoading && <LoadingSpinner className="my-24 mx-auto w-24" />}
+
+      {query.isSuccess && post && (
+        <>
+          <Post post={post} clickable={false} />
+          <div className="mb-4 p-4 text-white">Comments</div>
+
+          {post.comments.length == 0 && (
+            <span className="p-4 text-white/50">(No comments yet)</span>
+          )}
+
+          {post.comments.map((comment) => (
+            <div className="mb-6 pl-4" key={comment.id}>
+              <div className="flex items-center">
+                <Link to={`/profiles/${comment.author.name}`}>
+                  <div className="aspect-square w-8 overflow-hidden rounded-full">
+                    <img
+                      className="h-full w-full object-cover object-center"
+                      src={
+                        comment.author.avatar ||
+                        "../../public/img/default-profile-picture.png"
+                      }
+                    />
+                  </div>
+                </Link>
+
+                <Link
+                  to={`/profiles/${comment.author.name}`}
+                  className="ml-4 font-bold text-white"
+                >
+                  {comment.author.name}
+                </Link>
+                <Reply className="ml-4 h-4 fill-white"></Reply>
+                <span className="ml-1 text-white">
+                  <TimeAgo datetime={comment.created} />
+                </span>
+              </div>
+              <div className="mt-2 text-white">{comment.body}</div>
+            </div>
+          ))}
+        </>
+      )}
     </Layout>
   );
 };
